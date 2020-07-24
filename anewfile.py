eb911ed87c3f7b1c0c3d7cfb4cd0119f49d3a6b5
ameliawhit <68442551+ameliawhit@users.noreply.github.com>
2020-07-23 20:10:13 -0400

    newer edition. from 7/23 meeting
    goes to a website, finds all iframes, and commits each's source code to a text file (see file0-file4). doesn't pull exclusively ads, but does print source code.

--- anewfile.py
+++ anewfile.py
@@ -4,11 +4,25 @@ from selenium.webdriver.chrome.options import Options
 DRIVER_PATH = '/Users/awhitcomb/Desktop/chromedriver'
 driver = webdriver.Chrome(executable_path=DRIVER_PATH)
 
+driver.get('https://www.nytimes.com/2020/07/23/opinion/trump-chamber-of-commerce-lawsuit.html?action=click&module=Opinion&pgtype=Homepage')
+driver.implicitly_wait(10) #ads load
 all_iframes = driver.find_elements_by_tag_name("iframe")
 
+count = 0
 for iframe in all_iframes:
-   driver.switch_to_frame(iframe)
-   print(driver.page_source)
-   driver.switch_to_default_content()
-
+    filename = 'file' + str(count) + '.txt'
+    file = open(filename, "w")
+    driver.switch_to.frame
+    iframex = driver.page_source
+    file.write(iframex)
+    driver.switch_to_default_content()
+    file.close()
+    count += 1
+print(len(all_iframes))
 driver.quit()
+
+'''
+print('---------------------------------------------------')
+print(driver.page_source)
+print('---------------------------------------------------')
+'''
